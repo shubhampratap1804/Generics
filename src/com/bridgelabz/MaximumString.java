9fb99053d27654c711022274f23aa0370f1894c2
pratap1804 <pratap.1804@gmail.com>
2021-07-23 20:43:26 +0530

    Added: one generic method to find maximum of all 3 objects, each having 3 variables

--- src/com/bridgelabz/MaximumString.java
+++ src/com/bridgelabz/MaximumString.java
@@ -3,18 +3,31 @@ package com.bridgelabz;
 public class MaximumString {
 
 	// Main method
-	
+
 	public static void main(String[] args) {
 
-		//Declaring object variables
-		String x = "Delhi";
-		String y = "Mumbai";
-		String z = "Bengaluru";
+		// Declaring Integer object variables
+		Integer x = 5;
+		Integer y = 10;
+		Integer z = 15;
+
+		// Declaring Float object variables
+		Float a = 5.5f;
+		Float b = 5.6f;
+		Float c = 5.7f;
+
+		// Declaring String object variables
+		String p = "Delhi";
+		String q = "Mumbai";
+		String r = "Bengaluru";
+
 		printMax(x, y, z);
+		printMax(a, b, c);
+		printMax(p, q, r);
 	}
 
 	// Method to check which object variable is maximum
-	
+
 	public static <E extends Comparable<E>> E printMax(E x, E y, E z) {
 		E max = x;
 		if (y.compareTo(max) > 0) {
@@ -27,8 +40,8 @@ public class MaximumString {
 		return max;
 	}
 
-	//Method to print which object variable is maximum as a string
-	
+	// Method to print which object variable is maximum as a string
+
 	public static <E> void maximum(E x, E y, E z, E max) {
 		System.out.printf("Maximum of %s, %s and %s is %s \n", x, y, z, max);
 	}
